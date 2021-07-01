4192705ff354993fb0909052bed97bda3176c2a6
Osama <o.abuoun@gmail.com>
2021-07-01 20:47:15 +0100

    created a database

--- init-sql/01-create-db.sql
+++ init-sql/01-create-db.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE testing_db2;
+CREATE DATABASE calc_log_db;
+USE calc_log_db;
+CREATE TABLE requests (requestid INT NOT NULL AUTO_INCREMENT, operation VARCHAR(10), num1 FLOAT, num2 FLOAT, result FLOAT, PRIMARY KEY (requestid));
