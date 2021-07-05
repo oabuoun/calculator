9dd75cc8e3fb72e9011d7a5845ae92d79c6031b1
Osama <o.abuoun@gmail.com>
2021-07-05 17:31:43 +0100

    added html

--- calculator/main.py
+++ calculator/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 import calc_functions as calc_functions
 import db
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to the Calculator App"
+    return render_template("index.html")
 
 
 # URL: 127.0.0.1:5000/add/1/2
@@ -19,6 +19,29 @@ def welcome():
 def perform_add(number1, number2):
     number3 = calc_functions.add(number1, number2)
     return str(number3)
+
+
+# URL: 127.0.0.1:5000/add/1/2
+@app.route('/operation', methods=['GET', 'POST'])
+def perform_operation():
+    number1 = request.form.get('num1', type=int)
+    number2 = request.form.get('num2', type=int)
+    op      = request.form.get('op', type=str)
+    if number1 is None or number2 is None:
+        abort(400)
+
+    if op == '+':
+        result = calc_functions.add(number1, number2)
+    elif op == '-':
+        result = calc_functions.subtract(number1, number2)
+    elif op == '*':
+        result = calc_functions.multiply(number1, number2)
+    elif op == '/':
+        result = calc_functions.divide(number1, number2)
+    else:
+        result = "NULL"
+
+    return render_template("result.html", result = result, num1 = number1, num2 = number2, op = op)
 
 # Using Query String
 # URL: 127.0.0.1:5000/add?num1=1&num2=2
