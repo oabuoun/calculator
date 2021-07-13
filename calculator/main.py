b165938500fccdb01dfda47db1c8c55f5e10f904
Osama <o.abuoun@gmail.com>
2021-07-13 11:04:09 +0100

    cleaned the code

--- calculator/main.py
+++ calculator/main.py
@@ -40,38 +40,22 @@ def attempt_login():
 		'expiry': nowTime
 	}, 'SECRET_KEY_123456798', algorithm= 'HS256')
 
-	headers = {'Content-Type': 'text/html'}
-	#return make_response(render_template('login.html'), 200, headers, )
-
 	return render_template('login.html', username = username, token = token)
 
 @app.route('/dashboard', methods=['GET'])
 @decorators.token_required
 def dashboard(username):
-	print("-------------dashboard header ----------- ")
-
-	print(request.headers)
-	#run loging attempt
 	headers = {'Content-Type': 'text/html'}
 	return make_response(render_template('dashboard.html'), 200, headers)
-	#return render_template('dashboard.html', username = username)
 
 # URL: 127.0.0.1:5000/add/1/2
 @app.route('/op', methods=['POST'])
 @decorators.token_required
 def perform_operation(username):
-	print("------------- perform_operation ----------- ")
-
 	number1 = request.form.get('num1', type=int)
 	number2 = request.form.get('num2', type=int)
 	op	  = request.form.get('op', type=str)
 
-	print("------------- header ----------- ")
-
-	print(request.headers)
-	print("------------- Form Data ----------- ")
-
-	print(request.form)
 	if number1 is None or number2 is None or op is None:
 		return render_template("dashboard.html", num1 = number1, num2 = number2, op = op)
 
@@ -90,70 +74,7 @@ def perform_operation(username):
 		'result': result, 'num1': number1, 'num2': number2, 'op': op
 	}
 
-	print("result = {}".format(data))
 	return make_response(jsonify(data), 200)
 
-# URL: 127.0.0.1:5000/add/1/2
-@app.route('/add/<int:number1>/<int:number2>')
-def perform_add(number1, number2):
-	number3 = calc_functions.add(number1, number2)
-	return str(number3)
-
-# Using Query String
-# URL: 127.0.0.1:5000/add?num1=1&num2=2
-@app.route('/add')
-def perform_add_query():
-	number1 = request.args.get('num1', type=int)
-	number2 = request.args.get('num2', type=int)
-
-	if number1 is None or number2 is None:
-		abort(400)
-
-	result = calc_functions.add(number1, number2)
-	db.log("add", number1, number2, result)
-
-	return str(result)
-
-@app.route('/subtract')
-def perform_subtract_query():
-	number1 = request.args.get('num1', type=int)
-	number2 = request.args.get('num2', type=int)
-
-	if number1 is None or number2 is None:
-		abort(404)
-
-	number3 = calc_functions.subtract(number1, number2)
-	return str(number3)
-
-
-@app.route('/multiply')
-def perform_multiply_query():
-	number1 = request.args.get('num1', type=int)
-	number2 = request.args.get('num2', type=int)
-
-	if number1 is None or number2 is None:
-		abort(404)
-
-	number3 = calc_functions.multiply(number1, number2)
-	return str(number3)
-
-
-@app.route('/divide')
-def perform_divide_query():
-	number1 = request.args.get('num1', type=int)
-	number2 = request.args.get('num2', type=int)
-
-	if number1 is None or number2 is None:
-		abort(404)
-
-	number3 = calc_functions.divide(number1, number2)
-	return str(number3)
-
-
 if __name__ == '__main__':
-	#number1 = read_number(1)
-	#number2 = read_number(2)
-
-	#result = calc_functions.add(number1, number2)
-	#print(result)
 	app.run(debug=True, host='0.0.0.0', ssl_context = ('certs/pub_cert.pem', 'certs/private_key.pem'))
